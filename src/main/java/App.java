b99a386ac07e8a70f11c3a3d39b125c683bc5276
Gladkikh Olga <gladkikh.o21@gmail.com>
2022-08-09 00:36:55 +0300

    Add class Cat and cat-beans

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,7 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
@@ -8,5 +9,19 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        Cat firstBean = (Cat) applicationContext.getBean("cat");
+        System.out.println(firstBean.getName());
+
+        Cat secondBean = (Cat) applicationContext.getBean("cat");
+        System.out.println(secondBean.getName());
+
+        System.out.println(bean == bean2);
+        System.out.println(firstBean == secondBean);
+
     }
 }
